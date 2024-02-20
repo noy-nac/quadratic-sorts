79b1a41953a9647310d38d076a78a8006740dfc9
Canyon Mooney <cmooney@utexas.edu>
2024-02-20 17:52:57 -0600

    update starter code

--- QuadraticSorts.java
+++ QuadraticSorts.java
@@ -4,19 +4,6 @@ import java.util.*;
 public class QuadraticSorts {
 
     /**
-     * @pre none
-     * @post elements in array appear in ascending order
-     */
-    // TODO write a short description of how bubble sort works
-    //
-    // TODO state the big O of bubble sort and explain why
-    //
-    public static void bogoSort(int[] array) {
-        // TODO implement bogo sort
-
-    }
-
-    /**
      * @pre  0 <= i, j < array.length
      * @post array is modified to swap the elements at i and j
      */
@@ -26,13 +13,42 @@ public class QuadraticSorts {
     }
 
     /**
+     * @pre none
+     * @post elements in array appear in ascending order
+     */
+    // TODO write a short description of how bogo sort works
+    //
+    // TODO state the big O of bogo sort in the BEST, WORST, and AVERAGE case
+    // explain why
+    //
+    public static void bogoSort(int[] array) {
+        // TODO implement bogo sort (+1 point extra credit)
+
+    }
+
+    /**
+     * @pre none
+     * @post elements in array appear in ascending order
+     */
+    // TODO write a short description of how Boone sort works
+    //
+    // TODO state the big O of Boone sort in the BEST, WORST, and AVERAGE case
+    // explain why
+    //
+    public static void booneSort(int[] array) {
+        // TODO implement Boone sort (+1 point extra credit)
+
+    }
+
+    /**
      * @pre  none
      * @post elements in array appear in ascending order
      */
     // TODO write a short description of how bubble sort works
     //
-    // TODO state the big O of bubble sort and explain why
-    //
+    // TODO state the big O of bubble sort in the BEST, WORST, and AVERAGE case
+    // explain why
+    // 
     public static void bubbleSort(int[] array) {
         // TODO implement bubble sort
 
@@ -44,7 +60,8 @@ public class QuadraticSorts {
      */
     // TODO write a short description of how insertion sort works
     //
-    // TODO state the big O of insertion sort and explain why
+    // TODO state the big O of insertion sort in the BEST, WORST, and AVERAGE case
+    // explain why
     //
     public static void insertionSort(int[] array) {
         // TODO implement insertion sort
@@ -57,7 +74,8 @@ public class QuadraticSorts {
      */
     // TODO write a short description of how insertion sort works
     //
-    // TODO state the big O of selection sort and explain why
+    // TODO state the big O of selection sort in the BEST, WORST, and AVERAGE case
+    // explain why
     //
     public static void selectionSort(int[] array) {
         // TODO implement selection sort
